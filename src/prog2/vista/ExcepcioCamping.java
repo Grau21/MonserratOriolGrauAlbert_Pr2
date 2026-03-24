5cc1f3469ba44d5290ff075cc9dd2a065751a2bc
OctopusSquared <oriolmon@gmail.com>
2026-03-24 11:34:47 +0100

    Afegir les excepcions.

---
+++ src/prog2/vista/ExcepcioCamping.java
@@ -0,0 +1,10 @@
+
+package prog2.vista;
+
+/**
+ *
+ * @author Oriol Monserrat i Albert Grau.
+ */
+public class ExcepcioCamping extends Exception {
+    public ExcepcioCamping(String message) { super(message); }
+}
--- src/prog2/vista/ExcepcioReserva.java
+++ src/prog2/vista/ExcepcioReserva.java
@@ -6,9 +6,5 @@ package prog2.vista;
  * @author Oriol Monserrat i Albert Grau.
  */
 public class ExcepcioReserva extends Exception {
-
-    public ExcepcioReserva(String message) {
-        super(message);
-    }
-    
+    public ExcepcioReserva(String message) { super(message); }
 }
