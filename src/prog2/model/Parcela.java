89a3939ede324fbfd4c5e0029a05deb2dc7e8f4e
Grau21 <albertgrauperez@gmail.com>
2026-04-02 19:44:27 +0200

    Modificacions Parcela.java

--- src/prog2/model/Parcela.java
+++ src/prog2/model/Parcela.java
@@ -1,80 +1,24 @@
 package prog2.model;
 
 public class Parcela extends Allotjament {
-
-    // Atributs
-    //------------
-
     private float mida;
     private boolean connexioElectrica;
 
-    // Constructors
-    //---------------
-
-    /**
-     * Constructor per defecte de la classe.
-     * @param nom
-     * @param id
-     * @param estat
-     * @param illuminacio
-     * @param mida
-     * @param connexioElectrica
-     */
     public Parcela(String nom, String id, boolean estat, int illuminacio,
                    float mida, boolean connexioElectrica) {
-        super(nom, id, estat, illuminacio, 4, 2);
+        super(nom, id, estat, illuminacio, 4, 2);  // estada mínima: 4 dies baixa, 2 alta?
         this.mida = mida;
         this.connexioElectrica = connexioElectrica;
     }
 
-    // Getters i Setters.
-    //---------------------
-
-    /**
-     * Retorna la mida de la Parcela.
-     * @return
-     */
+    // Getters i setters (igual que abans)
     public float getMida() { return mida; }
-
-    /**
-     * Canvia la mida de la Parcela.
-     *
-     * @param mida
-     */
     public void setMida(float mida) { this.mida = mida; }
-
-    /**
-     * Retorna si hi ha connexió elèctrica en la Parcela (true) o no (false).
-     * @return
-     */
     public boolean isConnexioElectrica() { return connexioElectrica; }
-
-    /**
-     * Determina si hi ha connexió elèctrica en la Parcela.
-     *
-     * @param connexioElectrica
-     */
     public void setConnexioElectrica(boolean connexioElectrica) { this.connexioElectrica = connexioElectrica; }
 
-    // Mètodes
-    //---------
-
-    /**
-     * Retorna si la Parcela funciona correctament (true) o no (false).
-     * @return
-     */
-    @Override
-    public boolean correcteFuncionament() {
-        return connexioElectrica;
-    }
-
-    /**
-     * Retorna un String amb la informació de la Parcela.
-     * @return
-     */
     @Override
     public String toString() {
-        return super.toString() + " Parcela{mida=" + mida +
-                ", connexioElectrica=" + connexioElectrica + "}";
+        return super.toString() + ", Parcela{mida=" + mida + ", connexioElectrica=" + connexioElectrica + "}";
     }
 }
