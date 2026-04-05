432f87fab22d59a5dfebcf778ae3cdf705d23cb2
Grau21 <albertgrauperez@gmail.com>
2026-04-05 20:25:33 +0200

    Modificació AccesAsfalt.java

--- src/prog2/model/AccesAsfalt.java
+++ src/prog2/model/AccesAsfalt.java
@@ -3,29 +3,15 @@ package prog2.model;
 public abstract class AccesAsfalt extends Acces {
     private double metresQuadratsAsfalt;
 
-    /**
-     * Constructor per defecte de la classe.,
-     *
-     * @param nom
-     * @param estat
-     * @param metresQuadratsAsfalt
-     */
-    AccesAsfalt(String nom, boolean estat, double metresQuadratsAsfalt) {
+    public AccesAsfalt(String nom, boolean estat, double metresQuadratsAsfalt) {
         super(nom, estat);
         this.metresQuadratsAsfalt = metresQuadratsAsfalt;
     }
+    public double getMetresQuadratsAsfalt() { return metresQuadratsAsfalt; }
+    public void setMetresQuadratsAsfalt(double metresQuadratsAsfalt) { this.metresQuadratsAsfalt = metresQuadratsAsfalt; }
 
-    /**
-     * Retorna els metres quadrats d'asfalt de l'accés.
-     * @return
-     */
-    double getMetresQuadratsAsfalt() { return metresQuadratsAsfalt; }
-
-    /**
-     * Canvia els metres quadrats d'asfalt de l'accés.
-     * @param metresQuadratsAsfalt
-     */
-    void setMetresQuadratsAsfalt(double metresQuadratsAsfalt) {
-        this.metresQuadratsAsfalt = metresQuadratsAsfalt;
+    @Override
+    public String toString() {
+        return super.toString() + ", metres² asfalt=" + metresQuadratsAsfalt;
     }
 }
