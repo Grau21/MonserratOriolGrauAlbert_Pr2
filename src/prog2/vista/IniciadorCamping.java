66912a3aec0dfe2e49df8dff3311fe6042b16eb9
Grau21 <albertgrauperez@gmail.com>
2026-04-05 20:24:14 +0200

    Crear IniciadorCamping.java

---
+++ src/prog2/vista/IniciadorCamping.java
@@ -0,0 +1,8 @@
+package prog2.vista;
+
+public class IniciadorCamping {
+    public static void main(String[] args) {
+        VistaCamping vista = new VistaCamping("Green");
+        vista.gestioCamping();
+    }
+}
