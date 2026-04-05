91683d78ddbd8315ef4ea31936692996d8e46ede
Grau21 <albertgrauperez@gmail.com>
2026-04-05 20:27:16 +0200

    Modificació CamiTerra.java

--- src/prog2/model/CamiTerra.java
+++ src/prog2/model/CamiTerra.java
@@ -1,17 +1,13 @@
 package prog2.model;
 
-public class CarreteraAsfaltada extends AccesAsfalt {
-    private double pesMaxim;
-    public CarreteraAsfaltada(String nom, boolean estat, double metresQuadratsAsfalt, double pesMaxim) {
-        super(nom, estat, metresQuadratsAsfalt);
-        this.pesMaxim = pesMaxim;
+public class CamiTerra extends AccesTerra {
+    public CamiTerra(String nom, boolean estat, double longitud) {
+        super(nom, estat, longitud);
     }
     @Override
-    public boolean isAccessibilitat() { return true; }
-    public double getPesMaxim() { return pesMaxim; }
-    public void setPesMaxim(double pesMaxim) { this.pesMaxim = pesMaxim; }
+    public boolean isAccessibilitat() { return false; }
     @Override
     public String toString() {
-        return super.toString() + ", pes màxim=" + pesMaxim + " kg (Carretera asfaltada)";
+        return super.toString() + " (Camí de terra)";
     }
 }
