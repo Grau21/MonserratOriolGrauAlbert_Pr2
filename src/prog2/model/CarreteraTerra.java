38ecf12d879f197a2e3030e08820a742492696a9
Grau21 <albertgrauperez@gmail.com>
2026-04-05 20:28:17 +0200

    Modificació CarreteraTerra.java

--- src/prog2/model/CarreteraTerra.java
+++ src/prog2/model/CarreteraTerra.java
@@ -1,39 +1,13 @@
 package prog2.model;
 
-public class CarreteraTerra extends AccesTerra {
-    private boolean accesibilitat;
-    private double amplada;
-
-    /**
-     * Constructor per defecte de la classe.,
-     *
-     * @param nom
-     * @param estat
-     * @param longitud
-     * @param amplada
-     */
-    public CarreteraTerra(String nom, boolean estat, double longitud, double amplada) {
+public class CamiTerra extends AccesTerra {
+    public CamiTerra(String nom, boolean estat, double longitud) {
         super(nom, estat, longitud);
-        this.amplada = amplada;
-        accesibilitat = true;
     }
-
-    /**
-     * Retorna si l'accés permet accessibilitat amb cotxe o no.
-     * @return
-     */
-    public boolean isAccessibilitat() { return accesibilitat; }
-
-    /**
-     * Retorna l'amplada de l'accés.
-     * @return
-     */
-    public double getAmplada() { return amplada; }
-
-    /**
-     * Canvia l'amplada de l'accés.
-     *
-     * @param amplada
-     */
-    public void setAmplada(double amplada) { this.amplada = amplada; }
+    @Override
+    public boolean isAccessibilitat() { return false; }
+    @Override
+    public String toString() {
+        return super.toString() + " (Camí de terra)";
+    }
 }
