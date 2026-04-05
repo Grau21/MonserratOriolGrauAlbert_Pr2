78c43298bb7db450f82cd04a2501df677f4fb614
Grau21 <albertgrauperez@gmail.com>
2026-04-05 20:24:49 +0200

    Modificació Acces.java

--- src/prog2/model/Acces.java
+++ src/prog2/model/Acces.java
@@ -1,64 +1,29 @@
 package prog2.model;
 
-import java.util.ArrayList;
-
 public abstract class Acces implements InAcces {
-    // Atributs
     private String nom;
     private boolean estat;
-    private LlistaAllotjaments acces;
+    private LlistaAllotjaments allotjaments;
 
-    /**
-     * Constructor per defecte de la classe.
-     * @param nom
-     * @param estat
-     */
-    Acces(String nom, boolean estat) {
+    public Acces(String nom, boolean estat) {
         this.nom = nom;
         this.estat = estat;
+        this.allotjaments = new LlistaAllotjaments();  // inicialització important
     }
 
-    /**
-     * Afegeix un allotjament rebut com a paràmetre a la llista d'allotjaments de l'accés
-     * @param allotjament
-     */
     public void afegirAllotjament(Allotjament allotjament) {
-        acces.add(allotjament);
+        try { allotjaments.afegirAllotjament(allotjament); } catch (Exception e) {}
     }
-
-    /**
-     * Canvia l'estat de l'accés a tancat
-     */
-    public void tancarAcces() {
-        estat = false;
-    }
-
-    /**
-     * Canvia l'estat de l'accés a obert
-     */
-    public void obrirAcces() {
-        estat = true;
-    }
-
-    /**
-     * Retorna el nom de l'accés
-     * @return
-     */
+    public void tancarAcces() { estat = false; }
+    public void obrirAcces() { estat = true; }
     public String getNom() { return nom; }
-
-    /**
-     * Retorna l'estat de l'accés (obert o tancat)
-     * @return
-     */
     public boolean getEstat() { return estat; }
+    public LlistaAllotjaments getAAllotjaments() { return allotjaments; }
+    public abstract boolean isAccessibilitat();
 
-    /**
-     * Retorna la llista d'allotjaments associats a l'accés
-     * @return
-     */
-    public LlistaAllotjaments getAAllotjaments() { return acces; }
-
-    // Hem d'afegir el mètode "public abstract String toString()" que retorni la info dels atributs
-
-    // Podem afegir el mètode "public abstract isAccessibilitat()" per millor llegibilitat
+    @Override
+    public String toString() {
+        return "Accés " + nom + ", estat=" + (estat ? "Obert" : "Tancat") +
+               ", accessibilitat vehicle=" + (isAccessibilitat() ? "Sí" : "No");
+    }
 }
