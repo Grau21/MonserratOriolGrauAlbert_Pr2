283790cded3a1f5e57ca30a1fb9b546f1df5a998
Grau21 <albertgrauperez@gmail.com>
2026-04-05 20:27:49 +0200

    Modificació CarreteraAsfaltada.java

--- src/prog2/model/CarreteraAsfaltada.java
+++ src/prog2/model/CarreteraAsfaltada.java
@@ -1,35 +1,17 @@
 package prog2.model;
 
 public class CarreteraAsfaltada extends AccesAsfalt {
-    private boolean accesibilitat;
     private double pesMaxim;
-
-    /**
-     * Constructor per defecte de la classe.,
-     *
-     * @param nom
-     * @param estat
-     * @param metresQuadratsAsfalt
-     * @param pesMaxim
-     */
     public CarreteraAsfaltada(String nom, boolean estat, double metresQuadratsAsfalt, double pesMaxim) {
         super(nom, estat, metresQuadratsAsfalt);
         this.pesMaxim = pesMaxim;
-        accesibilitat = true;
     }
-
-    public boolean isAccessibilitat() { return accesibilitat; }
-
-    /**
-     * Retorna el pes màxim de l'accés.
-     * @return
-     */
-    public double getpesMaxim() { return pesMaxim; }
-
-    /**
-     * Canvia el pes màxim de l'accés.
-     *
-     * @param pesMaxim
-     */
-    public void setpesMaxim(double pesMaxim) { this.pesMaxim = this.pesMaxim; }
+    @Override
+    public boolean isAccessibilitat() { return true; }
+    public double getPesMaxim() { return pesMaxim; }
+    public void setPesMaxim(double pesMaxim) { this.pesMaxim = pesMaxim; }
+    @Override
+    public String toString() {
+        return super.toString() + ", pes màxim=" + pesMaxim + " kg (Carretera asfaltada)";
+    }
 }
